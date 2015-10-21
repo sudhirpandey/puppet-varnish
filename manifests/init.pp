137ef195fc922c9c19a9dbf305931233ff8ab204
Sudhir Pandey <sudhir@keystep048.keynett.no>
2015-10-21 11:32:43 +0200

    support for version  4.1

--- manifests/init.pp
+++ manifests/init.pp
@@ -30,7 +30,7 @@
 # [*storage_size*]
 #   Size of cache
 # [*varnish_version*]
-#   Major Varnish version to use - 3.0 or 4.0
+#   Major Varnish version to use - 3.[0-1] or 4.[0-1]
 # [*vcl_reload*]
 #   Script to use to load new Varnish config
 # [*package_ensure*]
@@ -68,7 +68,7 @@ class varnish (
   unless is_integer($max_threads) { fail('max_threads invalid') }
   validate_absolute_path($storage_file)
   validate_hash($runtime_params)
-  validate_re($varnish_version, '^[3-4]\.0')
+  validate_re($varnish_version, '^[3-4]\.[0-1]')
   validate_re($storage_type, '^(malloc|file)$')
 
   if ($addrepo) {
